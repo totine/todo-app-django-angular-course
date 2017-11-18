5d151449b959dbb62f9bac546a5fb6fbe5c8d65a
Joanna <joanna.gargas@gmail.com>
2017-11-18 16:04:33 +0100

    Add todo add and remove function in front

--- static/js/todo.js
+++ static/js/todo.js
@@ -1,4 +1,16 @@
 var app = angular.module('toDo', []);
 app.controller('toDoController', function ($scope) {
+    $scope.todoList = [{todoText: 'Finish this app', done: false}];
+    $scope.todoAdd = function () {
+        $scope.todoList.push({todoText: $scope.todoInput, done: false});
+        $scope.todoInput = '';
+    };
+    $scope.removeDoneTodos = function () {
+        var oldList = $scope.todoList;
+        $scope.todoList = [];
+        angular.forEach(oldList, function (x) {
+            if (!x.done) $scope.todoList.push(x);
+        });
+    };
     
 });
